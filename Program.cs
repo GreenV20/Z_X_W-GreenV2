d634d1e0ee543e6044e38ee088d94f7d18a27b14
GreenV20 <60959506+GreenV20@users.noreply.github.com>
2020-02-12 14:12:24 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace treugolnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первый угол");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второй угол");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третий угол");
+            int c = int.Parse(Console.ReadLine());
+            int result = a + b + c;
+            if (result == 180) 
+            {
+                Console.WriteLine("Такой треугольник есть!!");
+            }
+            else
+            {
+                Console.WriteLine("Такого треугольника нету");
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
